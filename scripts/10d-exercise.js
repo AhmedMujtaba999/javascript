4b8eccf823332770b9fa8d14d9270dfddb806c40
ahmed <mujtabaaliahmed559@gmail.com>
2026-07-04 01:34:22 +0530

    exercise file

---
+++ scripts/10d-exercise.js
@@ -0,0 +1,7 @@
+ function toggle(button){
+            if(button.classList.contains('is-toggled')){
+                button.classList.remove('is-toggled');
+            }else{
+                button.classList.add('is-toggled');
+            }
+        }
