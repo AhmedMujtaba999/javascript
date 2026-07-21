1aece153b1b8ec2b72dbdf38379028a6862beaa0
ahmed <mujtabaaliahmed559@gmail.com>
2026-07-21 22:01:43 +0530

    finding index

--- scripts/11o-exercise.js
+++ scripts/11o-exercise.js
@@ -1,10 +1,35 @@
-//const array =['hello', 'world', 'search', 'good']
-const array=['not', 'found'];
-for (let i=0; i<array.length;i++){
-if(array[i]==='search'){
-    console.log(i);
+// const array = ['hello', 'world', 'search', 'good', 'search']
+// //const array=['not', 'found'];
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'search') {
+//         console.log(i);
+//         break;
+//     }
+//     // else if(array[i]!=='search'){
+//     // console.log('-1');
+//     // }
+// }
+
+function findIndex(array, word) {
+    const search = word;
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] === search) {
+            return i;
+            break;
+        }
+       
+    }
+     for (let i = 0; i < array.length; i++) {
+
+        if (array[i] !== search) {
+            return -1;
+            break;
+        }
+       
+    }
+
 }
-if(array[i]!=='search'){
-console.log('-1');
-}
-}
+
+// console.log(findIndex(['green', 'red', 'blue', 'red'], 'red'));
+console.log(findIndex(['green', 'red', 'blue', 'red'], 'yellow'));
