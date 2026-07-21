f7cbecfb3a8e6ee8998545ed64e73cdf068eaf1d
ahmed <mujtabaaliahmed559@gmail.com>
2026-07-21 21:42:11 +0530

    exercise on array and loops js

---
+++ scripts/11o-exercise.js
@@ -0,0 +1,10 @@
+//const array =['hello', 'world', 'search', 'good']
+const array=['not', 'found'];
+for (let i=0; i<array.length;i++){
+if(array[i]==='search'){
+    console.log(i);
+}
+if(array[i]!=='search'){
+console.log('-1');
+}
+}
